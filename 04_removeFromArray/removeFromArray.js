95a38f54b6739c061d83ba8e6476514f592f9e40
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2024-01-16 21:06:01 +0100

    Add exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...nums) {
+  let responseArray = []
+  for (let i = 0; i < array.length; i++){
+    if (!nums.includes(array[i])){
+      responseArray.push(array[i])
+    }
+  }
+  return responseArray
 };
 
 // Do not edit below this line
