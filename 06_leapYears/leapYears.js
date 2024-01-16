36335363c8e013b4b48ab7c309e53d22e538c206
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2024-01-16 23:15:08 +0100

    Add exercise 6 - to finish

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (year % 4 == 0) {
+    if (year % 400 == 0) { // Find the right condition
+      return true
+   } else {
+      return false
+   }
+   return true
+  }
+  return false
 };
 
+leapYears(2000)
 // Do not edit below this line
 module.exports = leapYears;
