940215207d497802187e2987c6bcf81ee95b35a3
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2024-01-16 20:27:41 +0100

    Add exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+  splitText = text.split("");
+  let reversedResult = "";
+  for (let i = splitText.length - 1; i >= 0; i--) {
+    reversedResult += splitText[i]
+  }
+  return reversedResult
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
