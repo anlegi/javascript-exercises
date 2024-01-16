963d15cdf5de3a9ddde0dce1802ba951e1424f1f
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2024-01-16 19:43:05 +0100

    Add exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let repeatedString = "";
+  do {
+    if (num === 0) {
+      return ""
+    } else if (num < 0) {
+      return "ERROR"
+    } else {
+      repeatedString += string
+      num--;
+    }
+  } while (num > 0);
+  return repeatedString;
+}
 
-};
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
