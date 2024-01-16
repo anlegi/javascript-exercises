fd83d5857f6fcb451482cd3c8aba6c986e10ba89
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2024-01-16 22:21:10 +0100

    Add exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
 
+  if (x < 0 || isNaN(y) || (typeof y === "string") )  {
+    return 'ERROR'
+  }
+
+
+  if (y < x) {
+    temp = x;
+    x = y;
+    y = temp;
+  }
+
+  let sum = 0
+  for (let i = x; i <= y ; i++) {
+    sum += i
+  }
+  return sum
 };
+
+sumAll(12, 4)
 
 // Do not edit below this line
 module.exports = sumAll;
