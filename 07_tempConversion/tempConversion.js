eb12114de9ea70438956ad7c3419e48da933667a
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2024-01-18 21:22:34 +0100

    Add exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let celsius = (x - 32) * 5 / 9;
+  return Math.round(celsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let fahreinheit = x * (9 / 5) + 32;
+  return Math.round(fahreinheit * 10) / 10
 };
 
 // Do not edit below this line
