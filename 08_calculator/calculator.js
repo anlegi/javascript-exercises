0272cefa0b5fda5c8fd4bf2e21b84b760081ae2b
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2024-03-04 22:20:03 +0100

    Add calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sum = 0
+  a.forEach((value) => sum += value)
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+let multiplication = 1
+ a.forEach((value) => multiplication *= value)
+ return multiplication
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+
+  if (a === 0 || a === 1)
+    return 1
+
+  let solution = 1
+  for (let i = a; i > 0; i--) {
+    solution *= i
+  }
+  return solution
 };
 
 // Do not edit below this line
