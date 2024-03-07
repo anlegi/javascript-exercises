229eacdc94327238851cb1e408376bc2a7cb0ef9
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2024-03-07 21:07:30 +0100

    Add get titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,28 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
+const getTheTitles = function(arr) {
+  let arrTitle = []
+
+  // How to access all keys and values of an object:
+  // console.log("All keys: ", Object.keys(arr[1]))
+  // console.log("All values: ", Object.values(arr[1]))
+
+  arr.forEach(book => {
+    arrTitle.push(book.title)
+  })
+  return arrTitle
 };
+
+console.log(getTheTitles(books))
 
 // Do not edit below this line
 module.exports = getTheTitles;
