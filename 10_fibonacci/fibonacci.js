b1306e9ddd2b88fa28cc6d4b864a90c9df834955
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2024-03-07 20:42:17 +0100

    Add fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
 
+let count
+if (typeof input !== "number") // when input is not a number
+  count = parseInt(input) // parseInt turns input into integer
+else
+  count = input // for integer cases
+
+if (count < 0) return "OOPS"
+if (count == 0) return count
+
+let n1 = 0, n2 = 1, nextNumber;
+
+for (let i = 2; i <= count; i++) { // starts from 2 bc we already have the first two terms
+  nextNumber = n1 + n2
+  n1 = n2
+  n2 = nextNumber
+}
+  return n2
 };
 
 // Do not edit below this line
